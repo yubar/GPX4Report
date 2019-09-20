4eac1a152100727d8672d982a099c45eb712b432
yubar <yu-bar@yandex.ru>
2019-09-20 17:31:07 +0300

    Implement points labels, cleanup

--- profile.R
+++ profile.R
@@ -9,13 +9,6 @@
 #setwd("d:\\GH\\scripts\\EleProfile")
 #setwd("c:\\Git\\scripts\\EleProfile")
 
-##consts
-#ddy <- 0.02
-#colorDays <- "red3"
-#colorProfile <- "steelblue4"
-
-#Sys.setlocale("LC_ALL","Russian.UTF8")
-
 cat("Loading packages... ")
 
 suppressMessages(library(plotKML))
@@ -37,8 +30,6 @@ option_list = list(
 
 opt_parser = OptionParser(option_list=option_list)
 opt = parse_args(opt_parser)
-
-opt$timezone
 
 config = yaml.load_file("config.yml")
 
@@ -67,96 +58,52 @@ days$avg[nrow(days)] =  (days$l[nrow(days)] + xmax)/2
 days$l[1] = NA
 
 points <- read_csv(file=opt$points)
+points$sep[is.na(points$sep)] <- ", "
+points$label <- paste(
+	gsub("\\\\n", "\\\n", points$Name)
+	, gsub("\\\\n", "\\\n", points$sep)
+	, points$Ele
+	, sep=""
+)
+points$xnudge <- as.numeric(points$xnudge)
+points$ynudge <- as.numeric(points$ynudge)
+points$xnudge[is.na(points$xnudge)] <- -0.8*(points$hjust[is.na(points$xnudge)]-0.5)*2
+points$ynudge[is.na(points$ynudge)] <- -20*(points$vjust[is.na(points$ynudge)]-0.5)*2
+lines <- points[!is.na(points$length),]
+lines$xmin <- lines$L + lines$length*lines$hjust
+lines$xmax <- lines$L + lines$length*(1-lines$hjust)
+
 
 library(ggplot2)
 
-getPosition <- function(L, length, xpos) {
-	return(list(xmin=L + length*(xpos-1), xmax=L + length*xpos))
-}
-
-getLabel <- function(pts) {
-	return(paste(points$Name, points$Ele, sep=", "))
-}
-
-getNudge <- function(pts) {
-	return(list(
-		x = (points$xpos-0.5)*2*config$Fontsize$POIs,
-		y = -5*(points$ypos-0.5)*2*config$Fontsize$POIs
-	))
-}
-
 ga <- ggplot()
 ga <- ga + geom_linerange(data=days, aes(x=days$l, ymin=ylim[1], ymax=days$ele), color=config$Colors$Days)
-ga <- ga + geom_line(data=track, aes(x=l, y=ele), alpha=0.5, size=1, color=config$Colors$Profile) + geom_line(data=track, aes(x=l, y=ele), alpha=1, size=0.5, color=config$Colors$Profile)
-ga <- ga + geom_errorbarh(data=points[points$xpos!=0.5,], aes(xmin=getPosition(L, length, xpos)$xmin, xmax=getPosition(L, length, xpos)$xmax, y=Ele), color=config$Colors$Days, height=0)
-ga <- ga + geom_point(data=points, aes(x=L, y=Ele), color=config$Colors$Days, shape=1)
-#ga <- ga + annotate("text", x = points$L, y = points$Ele, label = points$Name, color = config$Colors$Days, size = 3, hjust=points$xpos, vjust=points$ypos)
-#ga <- ga + geom_text(data=points, aes(x=L, y=Ele, label=getLabel(points)), color = config$Colors$Days, size = 3, hjust=points$xpos, vjust=points$ypos, nudge_x=20, nudge_y=20)
-#ga <- ga + geom_text(data=points, aes(x=L, y=Ele, label=getLabel(points)), color = config$Colors$Days, size = config$Fontsize$Days, hjust=points$xpos, vjust=points$ypos, nudge_x=getNudge(points)$x, nudge_y=getNudge(points)$y)
+ga <- ga + geom_line(data=track, aes(x=l, y=ele), alpha=0.5, size=1.5, color=config$Colors$Profile) + geom_line(data=track, aes(x=l, y=ele), alpha=1, size=1, color=config$Colors$Profile)
+ga <- ga + geom_errorbarh(data=lines, aes(xmin=lines$xmin, xmax=lines$xmax, y=Ele), color=config$Colors$POIs, height=0)
+ga <- ga + geom_point(data=points, aes(x=L, y=Ele), color=config$Colors$POIs, shape=1, size=config$Points$Size, stroke=config$Points$Stroke)
+ga <- ga + geom_text(data=points, aes(x=L, y=Ele, label=label), color = config$Colors$POIs, size = config$Fontsize$POIs, hjust=points$hjust, vjust=points$vjust, nudge_x=points$xnudge, nudge_y=points$ynudge, lineheight=1)
 ga <- ga + annotate("text", x = days$avg, y = ymin-dy*config$Misc$ddy/2, label = days$N, color = config$Colors$Days, size = config$Fontsize$Days)
+ga <- ga + annotate("text", x = days$l[2]/4, y = ymin+dy*config$Misc$ddy/3, label=config$Labels$Days, color=config$Colors$Days, size=config$Fontsize$Days, fontface="bold")
 ga <- ga + scale_x_continuous(limits=xlim, expand = c(0, 0), breaks=seq(0, xmax, by = 10))
 ga <- ga + scale_y_continuous(limits=ylim, expand = c(0, 0), breaks=seq(0, ymax, by = 100))
 ga <- ga + theme(
 	panel.margin = unit(0, "null")
-	, plot.margin = unit(c(1, 1, 0, 0), "mm")
-	, axis.title.x = element_text(hjust=1)
-	, axis.title.y = element_text(hjust=0.5)
+	, plot.margin = unit(c(0, 2, 0, 0), "mm")
+	, axis.title.x = element_text(hjust=1, size=config$Fontsize$Axis_title)
+	, axis.title.y = element_text(hjust=0.5,size=config$Fontsize$Axis_title)
+	, axis.line.x = element_line(color="black", size = 0.5)
+	, axis.line.y = element_line(color="black", size = 0.5)
+	, axis.text.x = element_text(size=config$Fontsize$Axis_text)
+	, axis.text.y = element_text(size=config$Fontsize$Axis_text)
+	, axis.ticks.length=unit(.25, "cm")
 )
 ga <- ga + labs(x = config$Labels$xAxis, y = config$Labels$yAxis)
-
-ga + geom_text(data=points, aes(x=L, y=Ele, label=getLabel(points)), color = config$Colors$Days, size = config$Fontsize$POIs, hjust=points$xpos, vjust=points$ypos, nudge_x=getNudge(points)$x, nudge_y=getNudge(points)$y)
 
 cat("Done\n")
 cat("Saving output... ")
 
-png(filename = opt$out, width=config$Dims$Width, height=config$Dims$Height, units = "px", type="cairo") #, bg = "transparent"
+png(filename = opt$out, width=config$Dims$Width, height=config$Dims$Height, units = "px", type="cairo")
 ga
 invisible(dev.off())
 
 cat("Completed.\n")
-
-
-#######
-
-
-
-# ga <- ga + geom_point(aes(x=110, y=2000), color=config$Colors$Days, shape=1)
-# ga <- ga + annotate("text", x = 110, y = 2000, label = "some long\ntext ", color = config$Colors$Days, size = 3, hjust=0.5, 1)
-
-# df = data.frame(x = 110, y = 2000, label = "some long\ntext")
-# ga + geom_text(data=df, aes(x = x, y = y, label = label), color = config$Colors$Days, size = 3, hjust=0.5, 1)
-
-#g + geom_text_repel(data=track, aes(x=l, y=ele, label=labels, color=config$Colors$Days, size=(if(labels=="") 0.125 else 3)), show.legend=FALSE, box.padding=0.125)
-# ga <- ga + theme(
-	# panel.grid.major = element_blank()
-	# , panel.grid.minor = element_blank()
-	# , panel.background = element_blank()
-	# , panel.margin = unit(0,"null")
-    # , plot.margin = rep(unit(0,"null"),4)
-	# , rect = element_rect(fill = "transparent")
-	# , axis.title.x = element_text(hjust=1)
-	# , axis.title.y = element_text(hjust=0.5)
-# )
-
-# summarise(group_map(group_by(track, as.Date(track$dt)), head(.x, 1)), x=l,ymin=500,ymax=ele)
-
-# geom_linerange(aes(x=10000,ymin=500, ymax=1700))
-# g + geom_linerange(aes(x=10000,ymin=500, ymax=1700))
-# for(d in days) {g <- g + geom_linerange(aes(x=d$l,ymin=500, ymax=d$ele))}
-
-# days_ <- data.frame(x=c(10000,50000),ymin=500, ymax=c(1500, 1700))
-# for(d in days_) {d$x}
-
-
-# for(i in c(1,2)) {g <- g + geom_linerange(aes(x=days_$x[i],ymin=days_$ymin[i], ymax=days_$ymax[i]))}
-
-
-# for(i in c(1,3)) {g2 <- g2 + geom_linerange(aes(x=20000*i*i,ymin=500, ymax=1000*i))}
-
-# g + geom_linerange(data = days_, aes(x=days_$x,ymin=days_$ymin, ymax=days_$ymin))
-
-# gs <- ggplot(days_)
-# gs + geom_linerange(aes(x=days_$x,ymin=days_$ymin, ymax=days_$ymax))
-
-
-# g + geom_linerange(data = days, aes(x=days$l,ymin=500, ymax=days$ele))
--- run.bat
+++ run.bat
@@ -1,2 +1,1 @@
-REM "c:\Program Files\R\R-3.6.1\bin\x64\Rscript.exe" -e "eval(parse('profile.R', encoding='UTF-8'))"
 "c:\Program Files\R\R-3.6.1\bin\x64\Rscript.exe" profile.R -z "Asia/Irkutsk"
